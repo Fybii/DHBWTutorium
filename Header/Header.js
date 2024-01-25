a10642c258022ad84ca0e36e72aa0bb4967afe3c
Fybii <Fabian.Peifer23@gmail.com>
2024-01-25 08:55:31 +0100

    Data

---
+++ Header/Header.js
@@ -0,0 +1,17 @@
+let header = `
+        <nav class="navbar">
+            <div id="logo" class="logo-grid">
+                <p class="logo-1">DH</p>
+                <p class="logo-2">BWTorium</p>
+            </div>
+            <div>
+                <a class="nav-item" href="../Homepage/Homepage.html">Home</a>
+                <a class="nav-item" href="../Subjects/Subject.html">Fächer</a>
+            </div>
+            <div class="login-div">
+                <button type="button" class="login-btn">Login</button>
+            </div>
+        </nav>
+        `;
+
+    document.getElementById("website-header").innerHTML = header;
