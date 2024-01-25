0b9e9d49c0f63bfc1185c4afdcf68ee53da20838
Fybii <156109720+Fybii@users.noreply.github.com>
2024-01-25 09:30:53 +0100

    Delete Footer directory

--- Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-let footer = `
-        <nav class="nav-footer">
-            <a class="footer-item" href="">Datenschutz</a>
-            <a class="footer-item" href="">Anmelden</a>
-            <a class="footer-item" href="">Selber posten</a>
-        </nav>
-        `;
-
-document.getElementById("website-footer").innerHTML = footer;
